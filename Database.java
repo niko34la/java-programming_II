55b91f919f93c1a964ce4f1e8c174f30a9bed611
nikos latsis <44159836+niko34la@users.noreply.github.com>
2018-11-05 01:30:46 +0200

    make 2 for type and index

---
+++ Database.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+public class Database {
+	public static void main(String args[]) {
+		ArrayList<ArrayList<String>> type = new ArrayList<ArrayList<String>>();
+		ArrayList<String> index = new ArrayList<String>();
+	}
+}
+
