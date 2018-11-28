b69bbc8786b49a19839c1804c80b10d3ff47524a
nikos latsis <44159836+niko34la@users.noreply.github.com>
2018-11-28 15:39:58 +0200

    a

--- src/main/java/org/apache/maven/java_programming_II/Menu.java
+++ src/main/java/org/apache/maven/java_programming_II/Menu.java
@@ -57,7 +57,6 @@ public class Menu implements Choice {
     }
   }
 
-  
   /**
  * Our menu.  
  */
---
+++ src/test/java/org/apache/maven/java_programming_II/ColumnsTest.java
@@ -0,0 +1,8 @@
+package org.apache.maven.java_programming_II;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ColumnsTest extends TestCase{
+
+}
